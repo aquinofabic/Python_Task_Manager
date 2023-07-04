b48e8dbd41da3b6f7e1ed3d318b8fbfe362f196c
DJ Fabic <130316665+aquinofabic@users.noreply.github.com>
2023-07-04 17:48:36 +0100

    Add files via upload

--- main_TM.py
+++ main_TM.py
@@ -1,35 +1,60 @@
-from kivy.config import Config
-Config.set('kivy','keyboard_mode','systemanddock')
+# from kivy.config import Config
+# Config.set('kivy','keyboard_mode','systemanddock')
 from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.pickers import MDDatePicker
 from datetime import datetime
 
-
-"""
-First class to open dialogue box where user inputs task
-
-__init__ is an OOP method, called everytime an object is created from a class,
-    lets the class intialize object's attributes.
-    uses keyword self to assign the values passed as arguments to the objects attributes. 
-    class Dog:
-    def __init__(self, dogBreed, dogEyeColour):    
-        self.breed = dog.Breed
-        self.eyecolour = dog.EyeColour  # now creating an object
-tomita = Dog("fox terrier","brown")
-
-"""
+# First class to open dialogue box where user inputs task
 class DialogContent(MDBoxLayout):
-    def __init__(self):
+    # the init function for class constructer
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.ids.date_text.text = datetime.now().strftime("%A %d %B %Y")
+        # set self.id's. date's text. text property inside date_text = ...
+        # strftime is string format time
 
+    # This function will show the date picker
+    def show_date_picker(self):
+        date_dialog = MDDatePicker()
+        date_dialog.bind(on_save = self.on_save)
 
+    # This function will get date and save in a friendly form
+    def on_save(self, instance, value, date_range):
+        date = value.strftime("%A %d %B %Y")
+        self.ids.date_text.text = str(date)
+
+# Add main App class
 class MainApp(MDApp):
+    # Flag for show_task_function
+    task_list_dialog = None
+    # This is the build function for setting theme
     def build(self):
         self.theme_cls.primary_palette = ("Orange")
 
+    # This is the show task function
+    def show_task_dialog(self):
+        if not self.task_list_dialog:
+            self.task_list_dialog = MDDialog(
+                title = "Create Task",
+                type = "custom",
+                content_cls = DialogContent()
+            )
+            self.task_list_dialog.open()
+
+    # Add tasks
+    def add_task(self, task, task_date):
+        print(task.text, task_date)
+
+    # Dialog closing function
+    def close_dialog(self, *args):
+        self.task_list_dialog.dismiss()
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     app = MainApp()
     app.run()
+
+
+# 31.02s
---
+++ main_TM_notes.py
@@ -0,0 +1,28 @@
+"""""
+PYTHON
+__init__ is an OOP method, called everytime an object is created from a class,
+    lets the class intialize object's attributes.
+    uses keyword self to assign the values passed as arguments to the objects attributes.
+    
+    class Dog:
+    def __init__(self, dogBreed, dogEyeColour):    
+        self.breed = dog.Breed
+        self.eyecolour = dog.EyeColour  # now creating an object
+tomita = Dog("fox terrier","brown")
+print("this dog is a" ,tomita.breed, "and eyes are", tomita.EyeColour)
+
+
+**kwargs means keyword arguments
+*args means arguments
+
+super() is an example of class inheritence.
+
+
+
+KIVY
+kivy's .bind(current = a_function(arg1, arg2)) method, when current property changes,
+it will call a_function with arguments arg1, arg2
+
+root function allows 
+"""""
+
