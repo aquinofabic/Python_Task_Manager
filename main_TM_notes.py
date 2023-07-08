174db0497fd8afc1b48eee1bc6bd3fb7123f055e
DJ Fabic <130316665+aquinofabic@users.noreply.github.com>
2023-07-08 18:47:51 +0100

    Update main_TM_notes.py

--- main_TM_notes.py
+++ main_TM_notes.py
@@ -11,15 +11,39 @@ __init__ is an OOP method, called everytime an object is created from a class,
 tomita = Dog("fox terrier","brown")
 print("this dog is a" ,tomita.breed, "and eyes are", tomita.EyeColour)
 
+___________________________________________________________________________________________
 
-**kwargs means keyword arguments
-*args means arguments
+Unpacking operators: (*) is used to unpack iterables or argument lists, (**) is used to unpack dictionaries.
+
+*args (for integers) and **kwargs (for keywords) allows you to pass multiple arguments/keyword arguments to a function. For example:
+"""
+def sum(a,b):
+    return a + b  # limited to only two arguments
+    
+
+def arg_sum(*args):
+    result = 0
+    # Iterating over python args tuple
+    for x in args:
+        result += x
+    return result
+    
+print(arg_sum(1,2,3))
+
+"""
+Note: args is just a name and * is the unpacking operator.
+You are able to replace *args with, for example, *integers.
+
+Note: the iterable object you'll get using the unpacking operator (*) is not a list, but a tuple! (lists [] are mutable, tuples () are not)
+(**) can pass keyword arguments and returns both the key:value
+
+___________________________________________________________________________________________
 
 super() is an example of class inheritence.
 
 markup modules provides unified interface for 
 
-
+___________________________________________________________________________________________
 
 KIVY
 kivy's .bind(current = a_function(arg1, arg2)) method, when current property changes,
